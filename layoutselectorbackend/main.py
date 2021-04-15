c6ad3cb2023e52c1051ec1d6730fbb69c232254a
Raul Rodrigo Segura <raurodse@gmail.com>
2021-04-15 16:46:45 +0200

    add first version of backend

--- layoutselectorbackend/main.py
+++ layoutselectorbackend/main.py
@@ -1,26 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# === This file is part of Calamares - <https://github.com/calamares> ===
 #
-#   Copyright 2014-2015, Philip Müller <philm@manjaro.org>
-#   Copyright 2015-2017, Teo Mrnjavac <teo@kde.org>
-#   Copyright 2017, Alf Gaida <agaida@siduction.org>
-#   Copyright 2017, Adriaan de Groot <groot@kde.org>
-#   Copyright 2017, Gabriel Craciunescu <crazy@frugalware.org>
+#   Copyright 2021, Raul Rodrigo Segura <raurodse@gmail.com>
 #
-#   Calamares is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-#
-#   Calamares is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
 
 import libcalamares
 import os
@@ -37,8 +20,7 @@ def system_addons(config):
 
 def run():
     """
-    Create ubiquity modifications
-    :return:
+        
     """
     config = libcalamares.globalstorage.value('desktoplayout')
     return system_addons(config)
