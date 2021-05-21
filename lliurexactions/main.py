db0293993501a2c523c64d0135594999e008e734
Raul Rodrigo Segura <raurodse@gmail.com>
2021-05-21 13:32:08 +0200

    add last actions

--- lliurexactions/main.py
+++ lliurexactions/main.py
@@ -1,66 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
+#   Copyright 2018, Raul Rodrigo Segura <raurodse@gmail.com>
 #
-#   Copyright 2021, Raul Rodrigo Segura <raurodse@gmail.com>
-#
+#   GPLv3
 
 import libcalamares
-import os
+import subprocess
+import abc
+from pathlib import Path
 
-def system_addons(config):
-    '''
-        Set default desktop layout
-    '''
-    layout = 'default'
-    if config['layout'] == 'classic':
-        layout = 'classic'
-    libcalamares.utils.target_env_call(['llx-desktop-layout','set',layout])
-    return None
+from libcalamares.utils import check_target_env_call, target_env_call
 
 def run():
-    """
-        
-    """
-    config = libcalamares.globalstorage.value('lliurexDesktopLayout')
-    return system_addons(config)
-
-def system_addons( rmp, config, pkgman ):
-    '''
-        Create link to resolv.conf
-    '''
+    root = Path(libcalamares.globalstorage.value("rootMountPoint"))
     
-    analytics_path = Path("{rootmountpoint}/etc/lliurex-analytics/status".format(rootmountpoint=rmp)
-    analytics_path.parent.mkdir(exist_ok=True, parents= True)
-    with analytics_path.open("w",encoding='utf-8') as fd:
-        if config['statistics']:
-            # Enable Statistics
-            fd.write('yes\n')
-        else:
-            fd.write('no\n')
+    live_list_path = root.joinpath('etc','apt','sources.list.d','live.list')
+    opensshserver_path = root.joinpath('lib','systemd','system','ssh.service')
 
-    if config['inventory']:
-        pkgman.install('fusioninstall')
+    if live_list_path.exists():
+        live_list_path.unlink()
 
-    return None
+    if opensshserver_path.exists():
+        target_env_call(['dpkg-reconfigure','openssh-server'])
 
-
-
-
-def run():
-    if libcalamares.globalstorage.value("hasInternet"):
-        pkgman = PMApt()
-        packages = get_list_packages()
-        pkgman.update_db()
-        for package in packages :
-            try:
-                pkgman.install(package)
-            except subprocess.CalledProcessError:
-                libcalamares.utils.warning("Cound not install package {package}".format(package=package))
-    else:
-        libcalamares.utils.warning("Package installation has been skipped: no internet")
-
-    system_addons( libcalamares.globalstorage.value("rootMountPoint"), get_system_config(), pkgman) 
     return None
 
 
